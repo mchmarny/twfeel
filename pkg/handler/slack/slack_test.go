8b7f86dd345f21863ea1d8b1a9d2448661313481
Mark Chmarny <mark@chmarny.com>
2019-02-03 10:27:21 -0800

    slack test cleanup

--- pkg/handler/slack/slack_test.go
+++ pkg/handler/slack/slack_test.go
@@ -18,10 +18,10 @@ func setupRouter() *gin.Engine {
 }
 
 func TestSlackHandler(t *testing.T) {
-	body1 := []byte(`token=DGAZAc5lF2gwnomWDXjsSwK9&team_id=T93ELUK42&team_domain=knative&channel_id=D93E7DHT6&channel_name=directmessage&user_id=U94GEEP9V&user_name=mchmarny&command=%2Fkfeel&text=knative&response_url=https%3A%2F%2Fhooks.slack.com%2Fcommands%2FT93ELUK42%2F540718281363%2FI4JCetnXwIE5Ryna7IMLX77Q&trigger_id=540718281763.309496971138.252b8586d3df8f40f1ff381ded8ec20d`)
+	body1 := []byte(`token=test123&team_id=T1&team_domain=knative&channel_id=C1&channel_name=directmessage&user_id=U1&user_name=u1&command=%2Fkfeel&text=knative&response_url=https%3A%2F%2Ftest.domain.com&trigger_id=1.2.3`)
 	submitSlackHandler(t, body1)
 
-	body2 := []byte(`token=DGAZAc5lF2gwnomWDXjsSwK9&team_id=T93ELUK42&team_domain=knative&channel_id=D93E7DHT6&channel_name=directmessage&user_id=U94GEEP9V&user_name=mchmarny&command=%2Fkfeel&text=knative&response_url=https%3A%2F%2Fhooks.slack.com%2Fcommands%2FT93ELUK42%2F540718281363%2FI4JCetnXwIE5Ryna7IMLX77Q&trigger_id=540718281763.309496971138.252b8586d3df8f40f1ff381ded8ec20d`)
+	body2 := []byte(`token=test123&team_id=T1&team_domain=knative&channel_id=C1&channel_name=directmessage&user_id=U1&user_name=u1&command=%2Fkfeel&text=knative&response_url=https%3A%2F%2Ftest.domain.com&trigger_id=1.2.3`)
 	submitSlackHandler(t, body2)
 }
 
